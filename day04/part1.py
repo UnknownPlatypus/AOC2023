4905518945799dd8f960ae39de9f7b2bb1ff4aa2
Thibaut Decombe <thibaut.decombe@gmail.com>
2023-12-10 14:34:52 +0100

    Day 4 - part 1

---
+++ day04/part1.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    print("\n")
+    tot = 0
+
+    for line in s.splitlines():
+        _, games = line.split(": ")
+        win, have = games.split(" | ")
+        win_numbers = {int(nb) for nb in win.split()}
+        numbers = [int(nb) for nb in have.split()]
+
+        game_score = 0
+        for number in numbers:
+            if number in win_numbers:
+                if not game_score == 0:
+                    game_score *= 2
+                else:
+                    game_score = 1
+        tot += game_score
+
+    return tot
+
+
+INPUT_S = """\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+"""
+EXPECTED = 13
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
